9384293f38382c7b24e6429781f8e79630f9a65a
Amit Kinor <amit.kinor@gmail.com>
2020-06-11 12:27:15 +0300

    add login / out template - not stable

--- users/views.py
+++ users/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        valid = form.is_valid()
-        if valid:
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
             return redirect('blog-home')
+            #dfdf
 
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
 
 
