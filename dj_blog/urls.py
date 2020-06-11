675c65730c4a59cf0f729dbad380f1398de99ed4
Amit Kinor <amit.kinor@gmail.com>
2020-06-11 14:46:04 +0300

    enhancing profile model and changing the core media route

--- dj_blog/urls.py
+++ dj_blog/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path, include
 from users import views as users_views
 
@@ -28,3 +31,6 @@ urlpatterns = [
     
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            #username = form.cleaned_data.get('username')
             messages.success(request,f'Congratulations & welcome. You can now watch premium content. ')
             return redirect('login')            
     else:
