efb4196e43c5d030c9b6de6b676e5c05a902b002
Amit Kinor <amit.kinor@gmail.com>
2020-06-11 13:15:45 +0300

    added login \ logout \ profile for logged in users

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -8,13 +9,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
-            #dfdf
-
+            messages.success(request,f'Congratulations & welcome. You can now watch premium content. ')
+            return redirect('login')            
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 
 
